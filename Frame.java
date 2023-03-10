1a6ef6f706ddf53f5642a4fbe5b43560a81e41af
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-03-10 18:22:05 +0100

    Add files via upload

--- Frame.java
+++ Frame.java
@@ -1,11 +1,13 @@
 import javax.swing.JFrame;
 
-public class Frame extends JFrame{
+public class Frame{
     private JFrame frame;
 
     public void initialize(){
+        frame = new JFrame();
         this.frame.setTitle("Frame");
         this.frame.setSize(500, 500);
+        this.frame.setLocationRelativeTo(null);
         this.frame.setResizable(false);
         this.frame.setVisible(true);
     }
