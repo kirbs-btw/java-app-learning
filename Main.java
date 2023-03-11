5171c20b04403577c4d30bbf5d69cebf6ce869c5
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-03-11 22:04:24 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,8 +10,21 @@ public class Main {
             @Override
             public void run() {
                 Frame frame = new Frame();
+                JPanel panel = new JPanel();
+
+                panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+                frame.add(panel);
+
+                JButton button = new JButton("button");
+                button.setContentAreaFilled(false);
+                button.setBorder(new EmptyBorder(8, 8, 8, 8));
+
+
+                panel.add(button);
+
+
+
             }
         });
-
     }
 }
