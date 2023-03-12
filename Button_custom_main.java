56520127d2dde4481710c2d385da2f31c827e85f
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-03-12 12:12:27 +0100

    Add files via upload

---
+++ Button_custom_main.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Button_custom_main{
+    public JButton button;
+    public Button_custom_main(){
+        button = new JButton("ello");
+        button.setContentAreaFilled(false);
+        button.setBorder(new EmptyBorder(8, 8, 8, 8));
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                action();
+            }
+        });
+    }
+
+    public void action(){
+        System.out.println("ello sir");
+    }
+}
