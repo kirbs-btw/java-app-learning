5fc4cfe1d2d37a5a4ce05cb6e64ebe9b4f4990c7
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-03-13 18:40:20 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,8 +3,13 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class Main {
+
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -16,8 +21,18 @@ public class Main {
                 frame.add(panel);
 
                 Button_custom_main button = new Button_custom_main();
-
                 panel.add(button.button);
+
+                JTextField field = new JTextField(25);
+                panel.add(field);
+
+                button.button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println(field.getText());
+                    }
+                });
+
             }
         });
     }
