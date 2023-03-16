39d6668cbbbe4a1bc0f2ab67cbbeb36fff76ea0a
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-03-16 21:15:41 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -15,23 +15,30 @@ public class Main {
             public void run() {
                 Frame frame = new Frame();
                 JPanel panel = new JPanel();
+                panel.setBackground(Color.cyan);
 
-                panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
+                // panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+                panel.setLayout(new BorderLayout(5, 5));
                 frame.add(panel);
 
                 JLabel label = new JLabel("Hello Sir");
-                panel.add(label);
+                label.setBackground(Color.red);
+                panel.add(label, BorderLayout.WEST);
 
-                Button_custom_main button = new Button_custom_main();
-                panel.add(button.button);
+                Button_custom_main button = new Button_custom_main("Press me!");
+                panel.add(button.button, BorderLayout.SOUTH);
+
+                Button_custom_main buttontwo = new Button_custom_main("Press meeee!!");
+                panel.add(buttontwo.button, BorderLayout.CENTER);
 
                 JTextField field = new JTextField(25);
-                panel.add(field);
+                panel.add(field, BorderLayout.NORTH);
 
                 button.button.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        System.out.println(field.getText());
+                        System.out.println("Button said: " + field.getText());
                     }
                 });
 
